9277c9cf3dbfee7a31d65af44e89e57bdc99be69
padarshr <padarshr@codeaurora.org>
2017-07-12 17:32:34 +0530

    sm6250-common: Add device compatibility matrix
    Add device compatibility matrix to common project and
    targets can point the build variable DEVICE_MATRIX_FILE to
    this file to build device compatibility matrix.
    
    Change-Id: If8e75c5ee8de0ef50d6937ffbd6453fb3f5eb648

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 # Platform
 TARGET_BOARD_PLATFORM := atoll
