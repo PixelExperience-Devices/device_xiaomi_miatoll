0d171a099e7be7d24904db75f0cb6f0c7897345d
David Ng <dave@codeaurora.org>
2017-08-08 17:15:24 -0700

    sm6250-common: Add common config.fs AID/fs configuration file
    Common config.fs/$TARGET_FS_CONFIG_GEN AID/fs configuration
    file for all chipset inheriting from base.mk.
    
    Since legacy device-specific android_filesystem_config.h and
    config.fs do not co-exist, temporarily include config.fs only
    if legacy file is absent.  Once all legacy, device-specific
    files have been deleted, config.fs can be made unconditional.
    
    Change-Id: Ie100d0cd9db808b82d99be7139f0709390c7f3a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,9 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
--- common.mk
+++ common.mk
@@ -16,6 +16,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
