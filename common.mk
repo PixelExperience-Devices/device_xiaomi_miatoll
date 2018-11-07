8ed026cbc421b9677c187a9b8a08bbb83396278b
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    sm6250-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- common.mk
+++ common.mk
@@ -76,6 +76,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
