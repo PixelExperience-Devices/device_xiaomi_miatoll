f3796a363a4eb23e704a51700425cfda31101595
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    sm6250-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- common.mk
+++ common.mk
@@ -7,5 +7,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit proprietary targets
+$(call inherit-product-if-exists, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
