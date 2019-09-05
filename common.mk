5103f8908ca17445d1cbf1b1432710784f7ec583
Bruno Martins <bgcngm@gmail.com>
2019-09-05 12:17:15 +0100

    sm6250-common: Define Soong namespace
    Change-Id: Ibea0e8fe5dbc26d8953a9735f0b38cf087805b76

--- common.mk
+++ common.mk
@@ -15,3 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
