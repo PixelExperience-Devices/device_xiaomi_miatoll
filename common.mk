a6db72a1fce26e5c3d03d180fa92fd16c599727b
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 10:21:46 -0700

    sm6250-common: Use component-override to configure Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on sunfish devices.
    
    Remove usages of profile_supported_* from overlays as they no
    longer configure the enable state of Bluetooth componenets.
    
    Bug: 135048762
    Test:
          adb shell pm query-services \
          com.android.bluetooth/.a2dpsink.A2dpSinkService
          adb shell pm query-services \
          com.android.bluetooth/.avrcp.AvrcpTargetService
          adb shell pm query-services \
          com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
          adb shell pm query-services \
          com.android.bluetooth/.ba.BATService
          adb shell pm query-services \
          com.android.bluetooth/.hfpclient.HeadsetClientService
          adb shell pm query-services \
          com.android.bluetooth/.hid.HidDeviceService
          adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: If41df9fdefbcd952f8387451dc137565343e3d75

--- common.mk
+++ common.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Component override
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
@@ -261,14 +265,12 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
