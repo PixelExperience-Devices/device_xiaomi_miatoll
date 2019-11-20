3ab99f01c5f88dd712853c29458933a1d8494b5d
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-11-20 14:48:41 +0530

    miatoll: Enable full vndk support
    Change-Id: I1e50e8d66d79200410c19cca7ecd6248c89bd9d3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,6 +212,9 @@ TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 96
 # Side FP key for InputDispatcher to skip
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 96
 
+# Treble
+BOARD_VNDK_VERSION := current
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
--- device.mk
+++ device.mk
@@ -435,6 +435,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
