05701732a5aeb8c4e9332c3018794ab5566e51cf
Mikhail Naganov <mnaganov@google.com>
2019-11-22 17:34:45 -0800

    sm6250-common: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Test: VtsHalAudioV6_0TargetTest VtsHalAudioEffectV6_0TargetTest
    Bug: 155591007
    Change-Id: If40ae28c224160728634736ee95590a779f905c4
    (cherry picked from commit f11963392c9afd79704822a5812ef4412f3810db)

--- common.mk
+++ common.mk
@@ -39,8 +39,8 @@ PRODUCT_PACKAGES += \
     libtinycompress
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl:32 \
-    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.audio@2.0-service
 
