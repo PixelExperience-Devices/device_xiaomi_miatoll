f7ff168a1da0f1cab981dd17403def5bb4c57e21
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    sm6250-common: Use 4GB dalvik-heap config
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- atoll.mk
+++ atoll.mk
@@ -23,5 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
