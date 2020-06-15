816b531571be6feb9f00eaa7a057e36d186068ef
Nitin Shivpure <nshivpur@codeaurora.org>
2020-06-15 10:48:05 +0530

    sm6250-common: Add vendor.qti.hardware.btconfigstore@2.0 into product package
    Add vendor.qti.hardware.btconfigstore@2.0 into product package.
    
    CRs-fixed: 2739162
    Change-Id: I36ca083bf333826ac8980541181aa52022671396

--- common.mk
+++ common.mk
@@ -97,7 +97,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
