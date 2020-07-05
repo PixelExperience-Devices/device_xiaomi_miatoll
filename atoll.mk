3e433303bfeae44242568f4971d3556a9219f9fb
Demon000 <demonsingur@gmail.com>
2020-07-06 02:33:40 +0300

    sm6250-common: Inherit proprietary blobs
    Change-Id: Ib182c1a70449097e332c1233c7909fc419b65091

---
+++ atoll.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit proprietary targets
+$(call inherit-product, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
