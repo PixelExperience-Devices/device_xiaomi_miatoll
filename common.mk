f36a101882e1bff13b656784a6914d4baf31b529
Demon000 <demonsingur@gmail.com>
2020-07-06 04:11:05 +0300

    sm6250-common: inherit from base device configuration

---
+++ common.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
