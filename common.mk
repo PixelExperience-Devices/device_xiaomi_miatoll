7647273bb79bfce7d6889a426f24b2324b247d2b
Demon000 <demonsingur@gmail.com>
2020-07-11 14:46:28 +0300

    sm6250-common: include overlays

--- common.mk
+++ common.mk
@@ -30,6 +30,11 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
