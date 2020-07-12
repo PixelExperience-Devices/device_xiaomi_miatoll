f4100037a5e5b8ac9c315e07ad1bbe322e470d89
Demon000 <demonsingur@gmail.com>
2020-07-12 12:57:02 +0300

    [DNM] sm6250-common: permissive selinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
