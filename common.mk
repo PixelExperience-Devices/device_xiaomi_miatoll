e49eaee5fa9a94ecfc239817070044b00b461fe8
Demon000 <demonsingur@gmail.com>
2020-07-12 13:04:04 +0300

    sm6250-common: build telephony packages

--- common.mk
+++ common.mk
@@ -49,3 +49,16 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
