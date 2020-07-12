11d02d6e69c8fab0d3597349fffbb2de5c3f3ab7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-13 00:48:05 +0300

    sm6250-common: Don't build full super image
    Change-Id: I39e86181c3b57f614b61b339f0114100477862de

--- atoll.mk
+++ atoll.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
