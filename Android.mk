f56698ed1296b983ad83103f29d13d2d5d2c6942
Demon000 <demonsingur@gmail.com>
2020-08-15 00:50:59 +0300

    sm6250-common: Add miatoll to guard
    Change-Id: Iea179e6d66a2f4d5cf7bea95fb493e2f15eb7b68

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter placeholder,$(TARGET_DEVICE)),)
+ifneq ($(filter miatoll,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
