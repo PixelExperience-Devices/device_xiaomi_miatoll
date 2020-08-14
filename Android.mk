756f09f94be490e47966d65987bc2d520958a571
Demon000 <demonsingur@gmail.com>
2020-08-15 00:50:59 +0300

    sm6250-common: add miatoll to guard

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter curtana excalibur gram joyeuse,$(TARGET_DEVICE)),)
+ifneq ($(filter curtana excalibur gram joyeuse miatoll,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
