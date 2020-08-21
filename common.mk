8ef62933116ba6c5086915d925508dd33f75fc82
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    sm6250-common: Add Tethering Config
    Tethering package is APEX and we need to create a RRO to overwrite
    the values for config_tether_wifi_regexs and config_tether_wifi_p2p_regexs.
    BUG: 163800715
    
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- common.mk
+++ common.mk
@@ -339,6 +339,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
