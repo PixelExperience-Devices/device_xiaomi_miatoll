f585afc2ace8c61f6c7c70b85a4fa14fe0807e38
SonalSingh <sonal.singh.19993@gmail.com>
2020-08-24 12:51:49 +0300

    miatoll: prebuilts: Add GCamGOPrebuilt

--- device.mk
+++ device.mk
@@ -109,7 +109,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Snap
+    GCamGOPrebuilt
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
