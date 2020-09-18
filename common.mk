666e38496c59710f96071b3c343fc3195c318934
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-18 14:43:10 +0300

    sm6250-common: build clearkey from source

--- common.mk
+++ common.mk
@@ -79,6 +79,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.2-service.clearkey
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
