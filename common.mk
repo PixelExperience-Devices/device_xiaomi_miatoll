bbaf4103f4c23162856949b872408fdcf61175b7
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-04 07:52:27 +0300

    sm6250-common: build qti health@2.1 hal

--- common.mk
+++ common.mk
@@ -104,6 +104,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
