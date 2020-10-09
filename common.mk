bc47a90e2fad2c741c947d4e031acb76fe9d1c3a
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-09 11:04:29 +0300

    sm6250-common: add device-specific audio configs to device tree

--- common.mk
+++ common.mk
@@ -56,7 +56,7 @@ PRODUCT_PACKAGES += \
     libspkrprot
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
