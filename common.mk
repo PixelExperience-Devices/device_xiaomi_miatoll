27e4f91c338dbe6b1f622e6a9e9e8a965e64d967
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-23 13:47:12 +0300

    sm6250-common: build IR HAL from source

--- common.mk
+++ common.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml
 
 # IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
