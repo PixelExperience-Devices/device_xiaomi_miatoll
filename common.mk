2b220fa21890cac3c2c3cbe572d7ad868603fa34
LuK1337 <priv.luk@gmail.com>
2020-10-29 13:24:02 +0100

    sm6250-common: Enable RRO overlays for lineage-sdk and Snap
    Change-Id: I482f2393e10e90362db2c66b7abb0de837085843

--- common.mk
+++ common.mk
@@ -269,9 +269,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
