33d2f1582e9801a81f50186e1310ed6178063426
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-18 19:58:14 +0100

    sm6250-common: add servicetracker@1.2 interface to build packages

--- common.mk
+++ common.mk
@@ -315,7 +315,8 @@ PRODUCT_COPY_FILES += \
 # Servicetracker
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.0.vendor \
-    vendor.qti.hardware.servicetracker@1.1.vendor
+    vendor.qti.hardware.servicetracker@1.1.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
