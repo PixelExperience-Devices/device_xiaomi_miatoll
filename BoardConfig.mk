98aaa1c40c7dc7103c50116891e8e917d30419fd
Ramii Ahmed <ramy@ahmedramy.com>
2021-01-04 13:12:12 +0000

    miatoll: Enforcing bring up for R
    Co-authored-by: Aryan Gupta <guptaaryan189@gmail.com>
    Co-authored-by: Erfan Abdi  <erfangplus@gmail.com>
    Co-authored-by: Volodymyr Zhdanov <wight554@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,6 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += kpti=off
 
 # HIDL
