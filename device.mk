2703a890cfed912120f16721bad94b0dd2d823ed
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-07 19:24:04 +0530

    miatoll: Switch to source built vendor.qti.hardware.perf@2.2.so
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- device.mk
+++ device.mk
@@ -313,7 +313,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sm6250
+    android.hardware.power@1.3-service.xiaomi_sm6250 \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # RCS
 PRODUCT_PACKAGES += \
