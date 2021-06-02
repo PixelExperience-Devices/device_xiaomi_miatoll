5c1c6c9dc54791e46e7a3d9503d974510fe945c5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-03 00:27:07 +0200

    sm6250-common: Drop power AIDL extension
    Change-Id: Ied144ee0f7840ab43499d43f3d0c64dfd414d99f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,9 +149,6 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_VENDOR := xiaomi
 TARGET_BOARD_PLATFORM := atoll
 
-# Power
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
-
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
