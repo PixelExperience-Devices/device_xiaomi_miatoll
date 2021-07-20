a41b14b1bda7d5edf26bc2222fe28a8fbcdf965f
Alexander Winkowski <dereference23@outlook.com>
2021-07-20 14:08:14 +0200

    sm6250-common: Import powerhint.json
    * From sunfish-user 11 RQ3A.210705.001 7380771 release-keys
    
    Change-Id: I09399706d007a0e3de89945bd9fe269bd7fa78e3

--- atoll.mk
+++ atoll.mk
@@ -281,6 +281,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
