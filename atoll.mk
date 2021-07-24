278e6e50e787deb28a37282a9f733607af3d49cb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-24 07:29:23 +0200

    sm6250-common: Build IPACM libs
    Change-Id: Idd968a3ea54f1f7fda1e3579bdbce95b96349d3a

--- atoll.mk
+++ atoll.mk
@@ -176,6 +176,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
