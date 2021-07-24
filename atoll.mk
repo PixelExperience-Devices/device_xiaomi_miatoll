daf99d589e5c51ea079b2c2a43bd1104f531979f
Alexander Winkowski <dereference23@outlook.com>
2021-07-24 10:05:56 +0200

    sm6250-common: Import WiFi configs
    * From curtana-user 11 RKQ1.200826.002 V12.0.5.0.RJWINXM release-keys
    
    Change-Id: I8d6a32645274c3c17db08d2e20f8f4083233e95b

--- atoll.mk
+++ atoll.mk
@@ -396,5 +396,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
