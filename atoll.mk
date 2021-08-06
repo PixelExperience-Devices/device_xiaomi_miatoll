cefd104891bd857e62054353de5d48017d89658e
Alexander Winkowski <dereference23@outlook.com>
2021-08-06 08:29:39 +0200

    sm6250-common: thermal: Rebrand to Xiaomi SM6250
    Change-Id: Iafa72da14648a2652df9b64a02dd6e5b9fa8bd9b

--- atoll.mk
+++ atoll.mk
@@ -353,7 +353,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.pixel
+    android.hardware.thermal@2.0-service.xiaomi_atoll
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
