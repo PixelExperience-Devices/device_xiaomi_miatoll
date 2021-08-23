a99d14b72658c2fe45f0e1e9bfcfe883c004b506
Alexander Winkowski <dereference23@outlook.com>
2021-08-23 19:09:24 +0200

    sm6250-common: Move LineageOS HALs to a separate compatibility matrix
    Change-Id: If2f6d49cfc8609e15bc1ce5d725f54cdb91c5a4e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,7 +68,9 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/device_framework_matrix.xml \
+    $(COMMON_PATH)/lineage_device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
