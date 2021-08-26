3f85485d2d1f4eec757e7358cd8a814252590357
Bruno Martins <bgcngm@gmail.com>
2021-08-26 14:59:07 +0200

    sm6250-common: Build opensource soundtrigger HAL
    Change-Id: I956d53a67841a9e1e296de6b73f08ef2efac7470

--- atoll.mk
+++ atoll.mk
@@ -66,7 +66,8 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libvisualizer \
-    libvolumelistener
+    libvolumelistener \
+    sound_trigger.primary.atoll:32
 
 # Rootdir
 PRODUCT_PACKAGES += \
