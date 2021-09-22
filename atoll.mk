759befef559cbff362906bdb9b6506d077ca70b4
Alexander Winkowski <dereference23@outlook.com>
2021-09-22 17:31:50 +0200

    sm6250-common: Update sensors service to 2.1
    Change-Id: Ie417407c36929cf34ecbdd28feb1cab996fe1f30

--- atoll.mk
+++ atoll.mk
@@ -317,7 +317,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 # Servicetracker
