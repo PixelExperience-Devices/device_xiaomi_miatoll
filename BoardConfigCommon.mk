c72f1f81fdb7789a0a2ab43c51cf14302a4c3cb2
Alexander Winkowski <dereference23@outlook.com>
2021-12-27 11:54:36 +0000

    sm6250-common: Update blobs from V12.5.1.0.RJWMIXM
    Change-Id: I38a50bdefebe3074266f02e8fb3ab30043a72dc7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,7 +172,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-07-01
+VENDOR_SECURITY_PATCH := 2021-09-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
