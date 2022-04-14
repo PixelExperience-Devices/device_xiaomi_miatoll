0b6d328cf1c32d6b69a77ac856a7c8f4f8bbd5df
Alexander Winkowski <dereference23@outlook.com>
2022-04-14 20:16:50 +0100

    miatoll: Build LiveDisplay
    Change-Id: I05d9aaea5177f005885194c94b08642024bdc623

--- device.mk
+++ device.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
