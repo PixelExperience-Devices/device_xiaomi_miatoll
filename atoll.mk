165b7d83459a8468da1d5ef704a4668592f569e1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-01 01:26:49 +0200

    sm6250-common: Move to common Xiaomi fingerprint HIDL
    Change-Id: I6c29a831e985ee440c92a98aad8d33f25b9e5b2d

--- atoll.mk
+++ atoll.mk
@@ -138,7 +138,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_atoll
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 # FM
 PRODUCT_PACKAGES += \
