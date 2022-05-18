9c65a54dca9094f4fd6da69ce3a42c380aaf43af
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 09:43:16 +0200

    sm6250-common: Drop QTI camera device usage
    * qcom-camera topic hasn't been ported to 19.1
    * Keep building vendor.qti.hardware.camera.device@1.0 interface lib, IMS stack and possibly camera HAL still needs it
    
    Change-Id: I87bcd5b54ad986d5844df50de243fc1a18507198

--- atoll.mk
+++ atoll.mk
@@ -89,7 +89,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
