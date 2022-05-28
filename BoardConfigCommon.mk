813744ac12a158a0197e1564dd2e9668acf01ecc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:11:13 +0200

    sm6250-common: Remove TARGET_ENABLE_MEDIADRM_64
    * It's only used when building drm@1.0 HIDL
    
    Change-Id: I06412e2dd2bb48d798be4ef6e9a26a17b65277a7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,9 +58,6 @@ TARGET_USES_GRALLOC4 := true
 TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
