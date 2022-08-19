c9c3826f6731496755ca7ffa1aa58fb03e55b170
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:39:47 +0200

    miatoll: Use libhidlbase from VNDK v32 for Goodix interface
    * _ZN7android8hardware7details17gBnConstructorMapE and
      _ZN7android8hardware7details17gBsConstructorMapE have been
      removed in Android 13.
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I03ec2e4d709500cf01f8354cfba4dfa485b03293

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,10 @@ function blob_fixup() {
         vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
             sed -i "s/4\.0/4\.1/g" "${2}"
             ;;
+        vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+            ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
             "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
