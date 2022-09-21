3a4c4a73665675765cff368c92465ecafe2a4521
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    miatoll: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- device.mk
+++ device.mk
@@ -78,10 +78,6 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libstdc++.vendor
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
