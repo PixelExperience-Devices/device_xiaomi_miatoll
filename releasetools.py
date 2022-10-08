c2de0a2b783c59b52a09516368557f40ac64261d
Alexander Winkowski <dereference23@outlook.com>
2022-10-08 17:34:21 +0000

    miatoll: releasetools: Add firmware images to the output zip if present
    Change-Id: Iddbe5306379a5285018cd8e6b4c0a989b92caa11

--- releasetools.py
+++ releasetools.py
@@ -30,8 +30,23 @@ def AddImage(info, basename, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
+def AddRadioImage(info, basename, dest):
+  path = "RADIO/" + basename
+  if path not in info.input_zip.namelist():
+    return
+
+  data = info.input_zip.read(path)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+
+  AddRadioImage(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth")
+  AddRadioImage(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp")
+  AddRadioImage(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem")
+
   return
